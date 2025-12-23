07b44c44ce2e30469359a94be6add6d53121cfe1
Daniel Nugraha H.S <daniel.nugraha@aryanoble.co.id>
2025-12-23 14:35:32 +0700

    Update Kustomize

--- jenkins/k8s-manifest/jobdsl/seed.groovy
+++ jenkins/k8s-manifest/jobdsl/seed.groovy
@@ -2,19 +2,18 @@ import org.yaml.snakeyaml.Yaml
 
 def yaml = new Yaml()
 
-def goCfg = yaml.load(readFileFromWorkspace('projects/go.yaml'))
-goCfg.projects.each { p ->
-  evaluate readFileFromWorkspace('templates/go.groovy'), [
-    name: p.name,
-    port: p.port,
-    repo: p.repo
-  ]
+def go = yaml.load(new File('/var/jenkins_home/jobdsl/projects/go.yaml').text)
+go.projects.each { p ->
+  evaluate(
+    new File('/var/jenkins_home/jobdsl/templates/go.groovy').text,
+    [name: p.name, port: p.port, repo: p.repo]
+  )
 }
 
-def reactCfg = yaml.load(readFileFromWorkspace('projects/react.yaml'))
-reactCfg.projects.each { p ->
-  evaluate readFileFromWorkspace('templates/react.groovy'), [
-    name: p.name,
-    repo: p.repo
-  ]
+def react = yaml.load(new File('/var/jenkins_home/jobdsl/projects/react.yaml').text)
+react.projects.each { p ->
+  evaluate(
+    new File('/var/jenkins_home/jobdsl/templates/react.groovy').text,
+    [name: p.name, repo: p.repo]
+  )
 }
